ca81f07a975e94dc6632b2507105ace934b9481e
Nawang17 <nawangofficial1010@gmail.com>
2022-11-15 13:26:48 -0500

    forogot to change to prod url

--- src/api/config.js
+++ src/api/config.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   // testURL: "http://localhost:3001",
   //prodUrl: "https://momos-backend.onrender.com",
-  baseURL: `http://localhost:3001`,
+  baseURL: `https://momos-backend.onrender.com`,
 });

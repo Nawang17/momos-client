db77b00a8c9f72b88ddcd884258b15a227f4a555
Nawang17 <nawangofficial1010@gmail.com>
2023-10-17 13:45:06 -0400

    changed api config to prod url

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://momos-backend.onrender.com",
   // https://momos-backend.onrender.com
   //http://localhost:3001/
 });
